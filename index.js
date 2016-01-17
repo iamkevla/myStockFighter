fcad3279e5f0303d9488fe9dc612820f18435548
Kevin Vlahos <kevin.vlahos@gmail.com>
2016-01-17 16:44:28 +1100

    further improvements

--- index.js
+++ index.js
@@ -19,30 +19,20 @@ var quotes = [];
 
 const GET_QUOTE_INTERVAL = 1000;
 const SAMPLE_SIZE = 20;
+const ORDER_CHECK_INTERVAL = 5000;
+const ORDER_QUANTITY = 100;
 
-
-function getQuote() {
-    account.getQuote(stock)
-        .then(function(quote) {
-            quotes.push(quote);
-        });
-}
 
 var log = setInterval(getQuote, GET_QUOTE_INTERVAL);
 
-function stopLog() {
-    clearInterval(log);
-}
-
-
-
 Array.observe(quotes, function() {
+    var orderId;
     if (quotes.length === SAMPLE_SIZE) {
         // algorithm used to set buy price
         var price = parseInt((_.minBy(quotes, 'ask').ask + _.minBy(quotes, 'bid').bid) / 2, 10);
         quotes.splice(0, 10);
 
-        var orderId;
+
         /**
          * Buy some stock at predefined
          */
@@ -50,46 +40,73 @@ Array.observe(quotes, function() {
             .limitBuy({
                 stock: stock,
                 price: price,
-                quantity: 100
+                quantity: ORDER_QUANTITY
             })
             .then(function checkStatus(order) {
                 orderId = order.id;
-                console.log('Order Placed', order);
+                console.log('Order Placed', order.id, order.price);
                 return account.getOrderStatus(stock, order.id);
             })
             .then(function(resp) {
-                if (!resp.open) {
-                    stopLog();
-                    console.log('order completed', resp);
-                } else {
-                    //After ten seconds check status and cancel order if still open
-                    setTimeout(function() {
-                        //Check status of order is still open
-                        account.getOrderStatus(stock, orderId).then(function(resp) {
-                            console.log(['order status open :', resp.open]);
-                            if (resp.open) {
-                                //Cancel the order
-                                account.cancelOrder(stock, orderId)
-                                    .then(function(response) {
-                                        console.log('order cancelled');
-                                    })
-                                    .catch(function(err) {
-                                        console.log(err, ' ERROR');
-                                    });
+                return new Promise(function(resolve, reject) {
 
-                            } else {
-                                stopLog();
-                                console.log('order completed', resp);
-                            }
+                    function cleanupOnCompletion() {
+                        stopLog();
+                        console.log('order completed', resp);
+                        resolve();
+                    }
 
-                        });
+                    if (!resp.open) {
+                        cleanupOnCompletion();
+                    } else {
+                        //After ten seconds check status and cancel order if still open
+                        setTimeout(function() {
+                            //Check status of order is still open
+                            account.getOrderStatus(stock, orderId).then(function(resp) {
+                                console.log(['order status open :', resp.open]);
+                                if (resp.open) {
+                                    //Cancel the order
+                                    account.cancelOrder(stock, orderId)
+                                        .then(function(response) {
+                                            console.log('order cancelled');
+                                            resolve();
+                                        })
+                                        .catch(function(err) {
+                                            console.log(err, ' ERROR');
+                                            reject();
+                                        });
 
-                    }, 5000);
+                                } else {
+                                    cleanupOnCompletion();
+                                }
 
-                }
+                            });
+
+                        }, ORDER_CHECK_INTERVAL);
+
+
+                    }
+                });
+
+            })
+            .catch(function() {
+                console.log('There was an error!!');
             });
 
     } else if (quotes.length >= 1) {
-        console.log([quotes.length, _.minBy(quotes, 'ask') ? _.minBy(quotes, 'ask').ask : void(0)]);
+        process.stdout.write('.');
+        //console.log([quotes.length, _.minBy(quotes, 'ask') ? _.minBy(quotes, 'ask').ask : void(0)]);
     }
 });
+
+function stopLog() {
+    clearInterval(log);
+}
+
+function pushQuote(quote) {
+    quotes.push(quote);
+}
+
+function getQuote() {
+    account.getQuote(stock).then(pushQuote);
+}
